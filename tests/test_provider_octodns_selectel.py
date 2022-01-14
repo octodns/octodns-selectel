bf8f045651b8c0bd6df2fb39b5116414e50b9108
Ross McFarland <rwmcfa1@gmail.com>
2022-01-14 13:53:07 -0800

    Import skeleton

---
+++ tests/test_provider_octodns_selectel.py
@@ -0,0 +1,15 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_selectel import SelectelProvider
+
+
+class TestSelectelProvider(TestCase):
+
+    # TODO: test provider
+    def test_nothing(self):
+        self.assertTrue(True)
+        SelectelProvider
